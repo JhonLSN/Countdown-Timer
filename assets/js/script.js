8793816dcf5ba5e46c0953528048d2f3b2f81aea
JhonLSN <jhonatanluansantos@gmail.com>
2021-12-15 19:33:02 -0300

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,18 @@ const days = document.querySelector('.days')
 const hours = document.querySelector('.hours')
 const minutes = document.querySelector('.minutes')
 const seconds = document.querySelector('.seconds')
+
+let timeLeft = {
+  d: 0,
+  h: 0,
+  m: 0,
+  s: 0,
+}
+
+let totalSeconds;
+
+function init() {
+  totalSeconds = Math.floor((new Date('01.01.2022') - new Date()) / 1000); 
+}
+
+init();
